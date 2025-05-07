eb5fad3306c6103035546ae2b4e8d003552bed8e
Ben Wei <ben.stwei@gmail.com>
2025-05-06 21:46:01 -0700

    Update required vulkan version to 1.2

--- src/core/VulkanInstance.cpp
+++ src/core/VulkanInstance.cpp
@@ -35,7 +35,7 @@ void VulkanInstance::initInstance(Window& window) {
     appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 0, 1, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_API_VERSION(0, 0, 1, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_API_VERSION_1_2;
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
