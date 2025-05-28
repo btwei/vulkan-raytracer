3b608b06dbc8ea6acca7b8f192b2a61eec1ecfe7
Ben Wei <ben.stwei@gmail.com>
2025-05-27 22:32:50 -0700

    Add initial VulkanAllocator and AllocationBuffer classes

---
+++ include/vkrt/core/VulkanAllocator.hpp
@@ -0,0 +1,112 @@
+// Copyright (c) 2025 Benjamin Wei. All rights reserved.
+// This file is part of the vulkan-raytracer project.
+// Use, copying, modification, redistribution, or inclusion in training data (e.g. for AI) is strictly prohibited.
+
+#ifndef VKRT_VULKANALLOCATOR_HPP
+#define VKRT_VULKANALLOCATOR_HPP
+
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <vulkan/vulkan.h>
+
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
+
+#include "vkrt/core/VulkanInstance.hpp"
+#include "vkrt/core/VulkanDevice.hpp"
+#include "vkrt/core/VulkanCommandObjects.hpp"
+#include "vkrt/core/VulkanSyncObjects.hpp"
+
+namespace vkrt {
+
+class AllocatedBuffer;
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 uv;
+    glm::vec4 tangent;
+};
+
+struct MeshBuffer {
+    AllocatedBuffer buffer;
+    size_t indexOffset;
+    uint32_t indexCount;
+private:
+    MeshBuffer(AllocatedBuffer&& buf, size_t indexOffset, uint32_t indexCount)
+    : buffer(std::move(buf))
+    , indexOffset(indexOffset)
+    , indexCount(indexCount) { }
+
+    friend VulkanAllocator;
+};
+
+/**
+ * @class VulkanAllocator
+ * @brief Encapsulates memory allocation for buffers and images.
+ * 
+ * Provides functions for allocating memory such as for mesh allocations, 
+ * blas, tlas, or shader buffer tables. When allocating memory, returns 
+ * structs that contain AllocatedBuffer objects.
+ */
+class VulkanAllocator {
+public:
+    VulkanAllocator(const VulkanInstance& vulkanInstance, const VulkanDevice& vulkanDevice, const VulkanCommandPool& singleTimePool);
+    ~VulkanAllocator();
+
+    VulkanAllocator(const VulkanAllocator&) = delete;
+    VulkanAllocator& operator=(const VulkanAllocator&) = delete;
+
+    MeshBuffer uploadMesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices);
+    void copyBuffer(VkBuffer src, VkBuffer dst, VkDeviceSize size);
+
+private:
+    const VulkanInstance& vulkanInstance;
+    const VulkanDevice& vulkanDevice;
+    const VulkanCommandPool& singleTimePool;
+
+    VmaAllocator allocator;
+
+    void initAllocator();
+};
+
+/**
+ * @class AllocatedBuffer
+ * @brief RAII wrapper for a VkBuffer and VmaAllocation
+ * 
+ * Automatically destroys the buffer memory when out of scope. Can only
+ * be constructed by a VulkanAllocator object.
+ */
+class AllocatedBuffer {
+public:
+    ~AllocatedBuffer();
+
+    AllocatedBuffer(const AllocatedBuffer&) = delete;
+    AllocatedBuffer& operator=(const AllocatedBuffer&) = delete;
+
+    AllocatedBuffer(AllocatedBuffer&& other) noexcept
+    : buffer(other.buffer)
+    , allocation(other.allocation)
+    , allocator(other.allocator) {
+        other.buffer = VK_NULL_HANDLE;
+        other.allocation = VK_NULL_HANDLE;
+    }
+
+    const VkBuffer& getBuffer() const { return buffer; }
+
+private:
+    AllocatedBuffer(VkBuffer buffer, VmaAllocation allocation, VmaAllocator allocator);
+
+    VkBuffer buffer = VK_NULL_HANDLE;
+    VmaAllocation allocation = VK_NULL_HANDLE;
+    VmaAllocator allocator = VK_NULL_HANDLE;
+
+    friend class VulkanAllocator;
+};
+
+} // namespace vkrt
+
+#endif // VKRT_VULKANALLOCATOR_HPP
---
+++ src/core/VulkanAllocator.cpp
@@ -0,0 +1,117 @@
+// Copyright (c) 2025 Benjamin Wei. All rights reserved.
+// This file is part of the vulkan-raytracer project.
+// Use, copying, modification, redistribution, or inclusion in training data (e.g. for AI) is strictly prohibited.
+
+#include "vkrt/core/VulkanAllocator.hpp"
+
+namespace vkrt {
+
+VulkanAllocator::VulkanAllocator(const VulkanInstance& vulkanInstance, const VulkanDevice& vulkanDevice, const VulkanCommandPool& singleTimePool)
+: vulkanInstance(vulkanInstance)
+, vulkanDevice(vulkanDevice)
+, singleTimePool(singleTimePool)
+{
+    initAllocator();
+}
+
+VulkanAllocator::~VulkanAllocator() {
+    vmaDestroyAllocator(allocator);
+}
+
+MeshBuffer VulkanAllocator::uploadMesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices) {
+    size_t vertexBufferSize = sizeof(Vertex) * vertices.size();
+    size_t indexBufferSize = sizeof(uint32_t) * indices.size();
+
+    size_t indexBufferOffset = (vertexBufferSize + (4 - 1)) & ~(4 - 1); // Align to 4 bytes because of uint32_t indices
+
+    VkDeviceSize totalSize = indexBufferOffset + indexBufferSize;
+
+    VkBufferCreateInfo stagingBufferInfo = {VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO};
+    stagingBufferInfo.size = totalSize;
+    stagingBufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+
+    VmaAllocationCreateInfo allocCreateInfo {};
+    allocCreateInfo.usage = VMA_MEMORY_USAGE_AUTO;
+    allocCreateInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | 
+                            VMA_ALLOCATION_CREATE_MAPPED_BIT;
+    
+    VkBuffer stagingBuf;
+    VmaAllocation stagingAlloc;
+    VmaAllocationInfo stagingInfo;
+    vmaCreateBuffer(allocator, &stagingBufferInfo, &allocCreateInfo, &stagingBuf, &stagingAlloc, &stagingInfo);
+
+    memcpy(stagingInfo.pMappedData, vertices.data(), vertexBufferSize);
+    memcpy(stagingInfo.pMappedData + indexBufferOffset, indices.data(), indexBufferSize);
+
+    VkBufferCreateInfo localBufferInfo = {VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO};
+    localBufferInfo.size = totalSize;
+    localBufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
+                            VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
+                            VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+                            VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT;
+
+    VmaAllocationCreateInfo localAllocCreateInfo {};
+    localAllocCreateInfo.usage = VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE;
+
+    VkBuffer localBuf;
+    VmaAllocation localAlloc;
+    vmaCreateBuffer(allocator, &localBufferInfo, &localAllocCreateInfo, &localBuf, &localAlloc, nullptr);
+
+    copyBuffer(stagingBuf, localBuf, totalSize);
+
+    vmaDestroyBuffer(allocator, stagingBuf, stagingAlloc); // Clean up staging buffer
+
+    return MeshBuffer(AllocatedBuffer(localBuf, localAlloc, allocator), indexBufferOffset, indices.size());
+}
+
+void VulkanAllocator::copyBuffer(VkBuffer src, VkBuffer dst, VkDeviceSize size) {
+    VulkanCommandBuffer bufs = singleTimePool.createCommandBuffer();
+    VkCommandBuffer buf = bufs.getCommandBuffer();
+
+    VulkanFence fence(vulkanDevice);
+
+    VkCommandBufferBeginInfo beginInfo = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO};
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    vkBeginCommandBuffer(buf, &beginInfo);
+
+        VkBufferCopy copyRegion{};
+        copyRegion.size = size;
+        vkCmdCopyBuffer(buf, src, dst, 1, &copyRegion);
+
+    vkEndCommandBuffer(buf);
+
+    VkSubmitInfo submitInfo{};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &buf;
+
+    vkQueueSubmit(vulkanDevice.getPresentQueue(), 1, &submitInfo, fence.getFence());
+    vkWaitForFences(vulkanDevice.getDevice(), 1, &fence.getFence(), VK_TRUE, UINT64_MAX);
+}
+
+void VulkanAllocator::initAllocator() {
+    VmaVulkanFunctions vulkanFunctions = {};
+    vulkanFunctions.vkGetInstanceProcAddr = &vkGetInstanceProcAddr;
+    vulkanFunctions.vkGetDeviceProcAddr = &vkGetDeviceProcAddr;
+
+    VmaAllocatorCreateInfo allocatorInfo{};
+    allocatorInfo.vulkanApiVersion = VK_API_VERSION_1_2;
+    allocatorInfo.instance = vulkanInstance.getInstance();
+    allocatorInfo.physicalDevice = vulkanDevice.getPhysicalDevice();
+    allocatorInfo.device = vulkanDevice.getDevice();
+    allocatorInfo.pVulkanFunctions = &vulkanFunctions;
+
+    vmaCreateAllocator(&allocatorInfo, &allocator);
+}
+
+AllocatedBuffer::AllocatedBuffer(VkBuffer buffer, VmaAllocation allocation, VmaAllocator allocator)
+: buffer(buffer)
+, allocation(allocation)
+, allocator(allocator) { }
+
+AllocatedBuffer::~AllocatedBuffer() {
+    vmaDestroyBuffer(allocator, buffer, allocation);
+}
+
+} // namespace vkrt
