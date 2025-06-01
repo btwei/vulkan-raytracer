b750679f153f2aff8eeec6c84fa4a59ef972b69d
Ben Wei <ben.stwei@gmail.com>
2025-06-01 02:30:53 -0700

    Add pragmas to ignore expected Wunused-parameter, Wunused-variable, and Wmissing-field-initializer warnings

--- src/core/VmaUsage.cpp
+++ src/core/VmaUsage.cpp
@@ -1,4 +1,9 @@
 //VMA implementation file
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
+#pragma GCC diagnostic pop
